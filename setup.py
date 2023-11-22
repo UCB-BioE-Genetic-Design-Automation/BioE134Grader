c4357c509c9d170dd2a6bb1fe6fbc4aa442e63d1
J. Christopher Anderson <jcanderson@berkeley.edu>
2023-11-22 07:52:00 -0800

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BioE134Grader',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+        # For example: 'numpy', 'pandas'
+    ],
+    description='An autograder library for BioE134 course.'
+)
